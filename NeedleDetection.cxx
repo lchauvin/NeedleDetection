f4f4f43b7f4c3ddee13831dba98b26c44c5d11a5
Junichi Tokuda <tokuda@bwh.harvard.edu>
2012-12-04 10:24:36 -0500

    STYLE: Added author/copyright information.

--- NeedleDetection.cxx
+++ NeedleDetection.cxx
@@ -1,3 +1,18 @@
+/*=========================================================================
+
+  Program:   3D Slicer Needle Detection CLI
+  Language:  C++
+  Author:    Junichi Tokuda, Ph.D. (Brigham and Women's Hospital)
+
+  Copyright (c) Brigham and Women's Hospital. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
 #if defined(_MSC_VER)
 #pragma warning ( disable : 4786 )
 #endif
